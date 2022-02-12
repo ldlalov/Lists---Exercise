2206886203d09fa8bcd3efb52212a7742ab7daf1
Любомир Лалов <ldlalov@hotmail.com>
2022-02-12 10:09:27 +0200

    12022022

---
+++ 09. Pokemon Don't Go/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Pokemon_Don_t_Go
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> pokemons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int index = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (pokemons.Count > 0)
+            {
+                sum += pokemons[index];
+                int temp = pokemons[index];
+                if (pokemons.Count==0)
+                {
+                    break;
+                }
+                for (int i = 0; i < pokemons.Count; i++)
+                {
+                    if (pokemons[i] <= temp)
+                    {
+                        pokemons[i] += temp;
+                    }
+                    else
+                    {
+                        pokemons[i] -= temp;
+                    }
+                }
+                pokemons.RemoveAt(index);
+                index = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
