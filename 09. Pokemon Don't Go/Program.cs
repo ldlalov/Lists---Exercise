721b8c601b7ae89dd98811dce2407c9e4270eaac
Любомир Лалов <ldlalov@hotmail.com>
2022-02-12 15:54:12 +0200

    Ready

--- 09. Pokemon Don't Go/Program.cs
+++ 09. Pokemon Don't Go/Program.cs
@@ -6,35 +6,49 @@ namespace _09._Pokemon_Don_t_Go
 {
     internal class Program
     {
+        static void CathcPokemon(List<int> pokemons,int temp)
+        {
+            for (int i = 0; i < pokemons.Count; i++)
+            {
+                if (pokemons[i] <= temp)
+                {
+                    pokemons[i] += temp;
+                }
+                else
+                {
+                    pokemons[i] -= temp;
+                }
+            }
+        }
         static void Main(string[] args)
         {
             List<int> pokemons = Console.ReadLine().Split().Select(int.Parse).ToList();
-            int index = int.Parse(Console.ReadLine());
             int sum = 0;
             while (pokemons.Count > 0)
             {
-                sum += pokemons[index];
-                int temp = pokemons[index];
-                //if (index<0)
-                //{
-                //    pokemons[pokemons.Count - 1] = pokemons[0];
-                //}
-                //else if (index>pokemons.Count-1)
-                //{
-                //    pokemons[0] = pokemons.Count;
-                //}
-                for (int i = 0; i < pokemons.Count; i++)
+                int index = int.Parse(Console.ReadLine());
+                int temp = 0;
+                if (index < 0)
                 {
-                    if (pokemons[i] <= temp)
-                    {
-                        pokemons[i] += temp;
-                    }
-                    else
-                    {
-                        pokemons[i] -= temp;
-                    }
+                    temp = pokemons[0];
+                    pokemons[0] = pokemons[pokemons.Count - 1];
+                    CathcPokemon(pokemons, temp);
+                    sum += temp;
                 }
-                pokemons.RemoveAt(index);
+                else if (index > pokemons.Count - 1)
+                {            
+                    temp = pokemons[pokemons.Count - 1];
+                    pokemons[pokemons.Count - 1] = pokemons[0];
+                    CathcPokemon(pokemons, temp);
+                    sum += temp;
+                }
+                else
+                {
+                    temp = pokemons[index];
+                    pokemons.RemoveAt(index);
+                    sum += temp;
+                    CathcPokemon(pokemons, temp);
+                }
             }
             Console.WriteLine(sum);
         }
